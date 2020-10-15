517bda39d03d72b84ccb4b0b6ef68c99032aad25
JobMoreno-UDG <job.moreno7426@alumnos.udg.mx>
2020-10-15 13:25:42 -0500

    version 1.0

--- main.go
+++ main.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+	var i,valor,suma int
+	var s[] int
+	fmt.Scan(&i)
+	suma = 0
+	for x:=0;x<i;x++{
+		fmt.Scan(&valor)
+		s = append(s,valor)
+		suma = suma+valor
+	}
+	fmt.Print(suma) 
 }
